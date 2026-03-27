fb1a79eb75086d828ae9231c087de2905f930a2b
anothertoad <gnofnediaalt@gmail.com>
2026-03-26 22:37:45 -0400

    Update landscape.py

--- landscape.py
+++ landscape.py
@@ -3,18 +3,39 @@ import math
 
 pygame.init()
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 600
+HEIGHT = 482
 SIZE = (WIDTH, HEIGHT)
+#CENTER: 300, 241
 
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 
 # ---------------------------
 
-circle_x = 200
-circle_y = 200
 dt = 0
+startup_sound = [pygame.mixer.Sound("winxp.wav"), False]
+
+# ---------------------------
+
+def hills(surface):
+    points_top = []
+    for x in range(0, WIDTH):
+        offset = 20 * math.sin(x * 0.01 + dt / 5)
+        y = 282 + offset - 40 * math.sin((x+64)/150)
+        points_top.append((x, y))
+    points_top.append((WIDTH, HEIGHT))
+    points_top.append((0, HEIGHT))
+    pygame.draw.polygon(surface, (50, 200, 50), points_top)
+    
+    points_bot = []
+    for x in range(-50, WIDTH+50):
+        offset = 20 * math.sin(x * 0.01 + dt / 3)
+        y = 482 - (((x - 250)/120) ** 3 + 85 + offset)
+        points_bot.append((x, y))
+    points_bot.append((WIDTH+50, HEIGHT+50))
+    points_bot.append((-50, HEIGHT+50))
+    pygame.draw.polygon(surface, (50, 150, 50), points_bot)
 
 # ---------------------------
 
@@ -25,19 +46,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    # GAME STATE UPDATES
-    screen.fill(("#000000"))
-    circle_x += 10
-    circle_y = math.sin(circle_x / 40) * 100 + 200
-    pygame.draw.circle(screen, (0, 0, 255), (circle_x, circle_y), 30)
-    pygame.draw.circle(screen, (200, 200, 10), ((math.sin(dt)*200+240), (math.sin(dt)*200+240)), 20)
+    # GAME STATE UPDATES  
+    if dt < 3:
+        screen.fill("#000000")
+        if not startup_sound[1]:
+            pygame.mixer.Sound.play(startup_sound[0])
+            startup_sound[1] = True
+        pygame.draw.rect(screen, "#FF5C01", (190, 131, 100, 100))
+        pygame.draw.rect(screen, "#83C501", (310, 131, 100, 100))
+        pygame.draw.rect(screen, "#00B8F8", (190, 251, 100, 100))
+        pygame.draw.rect(screen, "#FFC000", (310, 251, 100, 100))
 
-    if circle_x > 800:
-        circle_x = -100
+    else:
+        screen.fill("#5581EE")
+        hills(screen)
+
     
     # UTILITIES
     pygame.display.flip()
-    dt += clock.tick(30)/1000
+    dt += clock.tick(30) / 1000
     #---------------------------
 
 pygame.quit()
